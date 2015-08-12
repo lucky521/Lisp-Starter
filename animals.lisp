72067cd7bd1cf7a1c63700c63761545e0a7651bc
Lucky <307479836@qq.com>
2015-08-12 09:01:24 +0800

    create animal guess

---
+++ animals.lisp
@@ -0,0 +1,48 @@
+
+#|
+lucky
+|#
+
+
+; start
+
+(defparamter animals-tree '("Does it live in the sea?" " a dolphin" "a horse") )
+
+
+
+(defun learn-animal (animal new-animal) ;two input
+  (let ((question
+         (capi:prompt-for-string
+          (format nil "Give me a yes/no question to distinguish between ~a and ~a"
+                  new-animal animal))))
+    (if (capi:prompt-for-confirmation
+         (format nil "What would the answer be for ~a? new-animal?"))
+        (list question new-animal animal))
+    (list question new-animal animal)))
+
+
+(defun ask-question (tree)
+  (if (capi:prompt-for-confirmation (first tree))
+      (list (first tree) (animals (second tree)) (third tree))
+    (list (first tree) (second tree) (animals (third tree)))))
+
+
+
+
+(defun make-guess (animal)
+  (if (capi:prompt-for-confirmation (format nil "Is it ~a?" animal))
+      (progn
+        (capi:display-message "Ho ho!")
+        animal)
+    (learn-animal 
+
+
+
+
+(defun annimals (tree)
+  (if (listp tree)
+      (ask-question tree)
+    (make-guess tree)))
+
+
+
